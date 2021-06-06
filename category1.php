d36b66b4c20050cbfb4c8119806314c031aae61b
ftmhalf <78713826+ftmhalf@users.noreply.github.com>
2021-06-06 11:05:10 +0700

    Update category1.php

--- category1.php
+++ category1.php
@@ -109,7 +109,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="pink" class="bi bi-heart-fill" viewBox="0 0 18 18">
                         <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
                     
-            <button type="submit" name= "favorite"value="<?php echo $dress_id; ?>"></button> 
+            <button type="submit" name= "favorite"value="$dress_id"></button> 
                     </svg>
                     </a>
               </div>
