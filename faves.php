662a3993364464f3790e70c98af338d7ccbb4012
ftmhalf <78713826+ftmhalf@users.noreply.github.com>
2021-06-06 13:57:52 +0700

    Update faves.php

--- faves.php
+++ faves.php
@@ -6,9 +6,10 @@ if(!isset($_SESSION['login'])){
 header('location:login.php');
 }else{
  $c_id = $_SESSION['user_id']; 
- $p_id = $_GET['dress_id'];
+ 
 
- $sql_Check = "SELECT * FROM fav where p_id = $p_id AND c_id = $c_id";
+ $sql_Check = "SELECT * FROM fav where c_id = $c_id";
+$p_id = $_GET['dress_id'];	
  $result_check = mysqli_query($conn, $sql_Check);
 
  if (mysqli_num_rows($result_check) == 1 ) { 
