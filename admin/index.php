a1a2003667282cc3c1d4990946bc49347151e8a3
ftmhalf <78713826+ftmhalf@users.noreply.github.com>
2021-06-06 22:38:39 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,12 @@
 
     <?php
 session_start();
+session_start();
+
+if( !isset($_SESSION["login"]) ) {
+   header("Location: login.php");
+   exit;
+}
 include("../functions.php");
 
 include "sidenav.php";
