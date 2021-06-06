507c8754d5aabda7848c23b8d3dddb91ba134cf9
ftmhalf <78713826+ftmhalf@users.noreply.github.com>
2021-06-06 14:37:06 +0700

    Update faves.php

--- faves.php
+++ faves.php
@@ -6,21 +6,20 @@ if(!isset($_SESSION['login'])){
 header('location:login.php');
 }else{
  $c_id = $_SESSION['user_id']; 
- 
-
- $sql_Check = "SELECT * FROM fav where c_id = $c_id";
-$p_id = $_GET['dress_id'];	
+ $p_id = $_GET['dress_id']; 
+	
+ $sql_Check = "SELECT * FROM fav where p_id=$p_id AND c_id = $c_id";
  $result_check = mysqli_query($conn, $sql_Check);
 
  if (mysqli_num_rows($result_check) == 1 ) { 
     echo 'product already exist in favorite';
     header('location:fav.php');
+	exit;
     
  }else{
     $insertfavorite = "INSERT INTO fav (p_id, c_id) VALUES ('$p_id', '$c_id')";   
 	if(mysqli_query($conn, $insertfavorite)){
         header('location:fav.php');
-
     }
 
  }
